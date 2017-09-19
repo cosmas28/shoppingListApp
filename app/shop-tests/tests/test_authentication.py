e5910d21bb4fa9c609a53b416ddad9319c145d3d
cosmas28 <augubilla100@gmail.com>
2017-09-19 15:58:15 +0300

    add sign-in test function

--- app/shop-tests/tests/test_authentication.py
+++ app/shop-tests/tests/test_authentication.py
@@ -1,3 +1,13 @@
 import os
 import unittest
 from ..app import Authentication
+
+class AuthenticationTest(unittest.TestCase):
+    """ validates users before they enter to their accounts"""
+    def test_sign_in(self):
+        submited_data = self.login('admin', 'default')
+        assert b'You were logged in' in submited_data.data
+        submited_data = self.login('adminx', 'default')
+        assert b'Invalid username' in submited_data.data
+        submited_data = self.login('admin', 'default')
+        assert b'Invalid password' in submited_data.data

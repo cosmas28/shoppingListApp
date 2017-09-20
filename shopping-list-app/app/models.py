225b1a865807469fe79374e37879355f37478399
cosmas28 <augubilla100@gmail.com>
2017-09-20 20:48:25 +0300

    modify initialization methods

--- shopping-list-app/app/models.py
+++
@@ -1,4 +0,0 @@
-# use data dictionary to store data
-
-sign_up_data = {'username': 'password1234'}
-sign_in_data = {'username': 'password1234'}
--- shopping-list-app/tests/test_back_end.py
+++ shopping-list-app/tests/test_back_end.py
@@ -7,7 +7,9 @@ class TestCase(TestCase):
 
     def setUp(self):
         """Define test variables and initialize app. """
-        pass
+        self.sign_up_data = {'username': 'admin', 'password': 'admin123'}
+        self.sign_in_data = {'username': 'admin', 'password': 'admin123'}
 
     def tearDown(self):
-        pass
+        del self.sign_up_data
+        del self.sign_in_data

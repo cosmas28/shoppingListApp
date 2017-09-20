192ba0241f3975d98e2f3eb7e3f441cba0bb68a3
cosmas28 <augubilla100@gmail.com>
2017-09-20 21:19:17 +0300

    add sign up test method

--- shopping-list-app/tests/test_back_end.py
+++ shopping-list-app/tests/test_back_end.py
@@ -9,7 +9,15 @@ class TestCase(TestCase):
         """Define test variables and initialize app. """
         self.sign_up_data = {'username': 'admin', 'password': 'admin123'}
         self.sign_in_data = {'username': 'admin', 'password': 'admin123'}
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
 
     def tearDown(self):
         del self.sign_up_data
         del self.sign_in_data
+
+    def test_sign_up(self):
+        """Test user can sign up """
+        pos = self.client().post('/signup/', data=self.sign_up_data)
+        self.assertEqual(pos.status_code, 201)
+        self.assertIn('admin', str(pos.data))

9b16f42a92dba82cd556cc435079fe4abb1205e9
cosmas28 <augubilla100@gmail.com>
2017-09-21 10:40:10 +0300

    import each blueprint object and registered it

--- shopping-list-app/app/__init__.py
+++ shopping-list-app/app/__init__.py
@@ -6,4 +6,13 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
 
+    from .user import user as user_blueprint
+    app.register_blueprint(user_blueprint, url_prefix='/')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
+
     return app
